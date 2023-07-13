75a3646844375ea457c6ff657ab0e0bf434369d6
Lustiyana <lustiyana1801@gmail.com>
2023-07-13 14:42:00 +0700

    Add responsive

--- src/pages/components/navbar/index.jsx
+++ src/pages/components/navbar/index.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
         <Loader />
       ) : (
         <>
-          <div className="dropdown dropdown-bottom">
+          <div className="dropdown dropdown-bottom sm:hidden">
             <label tabIndex={0} className="btn btn-ghost m-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
