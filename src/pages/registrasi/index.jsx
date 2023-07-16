d49cff0057984e3a156b8122af968cfe45080056
Lustiyana <lustiyana1801@gmail.com>
2023-07-16 20:08:56 +0700

    Add another angkatan

--- src/pages/registrasi/index.jsx
+++ src/pages/registrasi/index.jsx
@@ -144,6 +144,8 @@ export default function Registrasi() {
             onChange={formik.handleChange}
           >
             <option selected>Angkatan</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
             <option value="2019">2019</option>
             <option value="2020">2020</option>
             <option value="2021">2021</option>
